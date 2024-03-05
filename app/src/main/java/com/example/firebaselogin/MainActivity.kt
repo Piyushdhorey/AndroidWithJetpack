d548b18bb5673aea70514a2b8264e09c90ad0ec6
Piyushdhorey <pdhorey@cxr.agency>
2024-03-05 13:50:27 +0530

    Google sign in with firebase in compose

---
+++ app/src/main/java/com/example/firebaselogin/MainActivity.kt
@@ -0,0 +1,125 @@
+package com.example.firebaselogin
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.compose.setContent
+import androidx.activity.result.IntentSenderRequest
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.firebaselogin.presentation.sign_in.GoogleAuthClient
+import com.example.firebaselogin.presentation.sign_in.ProfileScreen
+import com.example.firebaselogin.presentation.sign_in.SignInScreen
+import com.example.firebaselogin.presentation.sign_in.SignInViewMode
+import com.example.firebaselogin.ui.theme.FirebaseLoginTheme
+import com.google.android.gms.auth.api.identity.Identity
+import kotlinx.coroutines.launch
+
+class MainActivity : ComponentActivity() {
+
+    private val googleAuthClient by lazy {
+        GoogleAuthClient(
+            context = applicationContext,
+            oneTapClient = Identity.getSignInClient(applicationContext)
+        )
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FirebaseLoginTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navController = rememberNavController()
+                    NavHost(navController = navController, startDestination = "sign_in") {
+                        composable("sign_in"){
+                            val viewModel = viewModel<SignInViewMode>()
+                            val state by viewModel.state.collectAsStateWithLifecycle()
+
+                            LaunchedEffect(key1 = Unit) {
+                                if(googleAuthClient.getSignedInUser() != null){
+                                    navController.navigate("profile")
+                                }
+                            }
+
+                            val launcher = rememberLauncherForActivityResult(contract = ActivityResultContracts.StartIntentSenderForResult(),
+                                onResult = {result ->
+                                    if (result.resultCode == RESULT_OK) {
+                                        lifecycleScope.launch {
+                                            val signInResult = googleAuthClient.signInWithIntent(
+                                                intentL = result.data ?: return@launch
+                                            )
+                                            viewModel.onSignInResult(signInResult)
+                                        }
+                                    }
+                                })
+
+                            LaunchedEffect(key1 = state.isSignInSuccessful) {
+                                if (state.isSignInSuccessful) {
+                                    Toast.makeText(
+                                        applicationContext,
+                                        "Sign in successful",
+                                        Toast.LENGTH_LONG
+                                    ).show()
+
+                                    navController.navigate("profile")
+                                    viewModel.resetState()
+                                }
+                            }
+
+                            SignInScreen(state = state, onSignInClick = {
+                                lifecycleScope.launch {
+                                    val signInIntent = googleAuthClient.signIn()
+                                    launcher.launch(
+                                        IntentSenderRequest.Builder(
+                                            signInIntent ?: return@launch
+                                        ).build()
+                                    )
+                                }
+                            })
+                        }
+
+                        composable("profile"){
+                            ProfileScreen(
+                                userData = googleAuthClient.getSignedInUser() ,
+                                onSignOut = {
+                                    lifecycleScope.launch {
+                                        googleAuthClient.signOut()
+                                        Toast.makeText(
+                                            applicationContext,
+                                            "Signed out",
+                                            Toast.LENGTH_LONG
+                                        ).show()
+
+                                        navController.popBackStack()
+                                    }
+                                })
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
